e650d2dd0d9c8c04045ca136964096a8cb31aaac
Balthasar Teuscher <balthasar.teuscher@tum.de>
2025-11-21 15:56:34 +0100

    Reuse target for rmse computation

--- weights.py
+++ weights.py
@@ -1,28 +1,12 @@
+import copy
 import numpy as np
 import open3d as o3d
 
-from data import np_point_cloud2_pcd, pcd2np_point_cloud
+from data import np_point_cloud2_pcd
 
 
-def preprocess_point_cloud(pcd, voxel_size):
-    pcd_down = pcd.voxel_down_sample(voxel_size)
-
-    radius_normal = voxel_size * 2
-    pcd_down.estimate_normals(
-        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30)
-    )
-
-    radius_feature = voxel_size * 5
-    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
-        pcd_down,
-        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100),
-    )
-    return pcd_down, pcd_fpfh
-
-
-def prepare_dataset(source, target, voxel_size):
-
-    trans_init = np.asarray(
+def prepare_dataset(pcd, voxel_size, is_target=False):
+    TRANS_INIT = np.asarray(
         [
             [0.0, 0.0, 1.0, 0.0],
             [1.0, 0.0, 0.0, 0.0],
@@ -30,17 +14,38 @@ def prepare_dataset(source, target, voxel_size):
             [0.0, 0.0, 0.0, 1.0],
         ]
     )
-    source.transform(trans_init)
 
-    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
-    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
-    return source, target, source_down, target_down, source_fpfh, target_fpfh
+    if not is_target:
+        pcd.transform(TRANS_INIT)
+
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    params = o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30)
+    pcd_down.estimate_normals(params)
+
+    radius_feature = voxel_size * 5
+    params = o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd_down, params)
+
+    return pcd, pcd_down, pcd_fpfh
 
 
-def execute_global_registration(
-    source_down, target_down, source_fpfh, target_fpfh, voxel_size
-):
+def global_registration(source_down, target_down, source_fpfh, target_fpfh, voxel_size):
     distance_threshold = voxel_size * 1
+    p2p_transformation_estimation = (
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False)
+    )
+    correspondance_checkers = [
+        o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+        o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+            distance_threshold
+        ),
+    ]
+    convergence_criteria = o3d.pipelines.registration.RANSACConvergenceCriteria(
+        100000, 0.999
+    )
+
     result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
         source_down,
         target_down,
@@ -48,40 +53,34 @@ def execute_global_registration(
         target_fpfh,
         True,
         distance_threshold,
-        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        p2p_transformation_estimation,
         3,
-        [
-            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
-                distance_threshold
-            ),
-        ],
-        o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999),
+        correspondance_checkers,
+        convergence_criteria,
     )
+
     return result
 
 
-def compute_rmse(lst_PC, lst_acc, voxel_size):
-    PC_list = lst_PC.copy()
-    target = np_point_cloud2_pcd(PC_list.pop(lst_acc.index(min(lst_acc))))
-    rmse = []
-    lst_pcs = []
-    PC_list = lst_PC.copy()
-    for pc in PC_list:
-        source = np_point_cloud2_pcd(pc)
+def compute_rmse(point_clouds_pcd, accuracies, voxel_size):
+    # prepeare target
+    target_pcd = point_clouds_pcd[accuracies.index(min(accuracies))]
+    target_pcd = copy.deepcopy(target_pcd)
+    target_pcd, target_down, target_fpfh = prepare_dataset(
+        target_pcd, voxel_size, is_target=True
+    )
 
-        source, target, source_down, target_down, source_fpfh, target_fpfh = (
-            prepare_dataset(source, target, voxel_size)
-        )
-        result_ransac = execute_global_registration(
+    rmse = []
+
+    for source_pcd in point_clouds_pcd:
+        (source_pcd, source_down, source_fpfh) = prepare_dataset(source_pcd, voxel_size)
+
+        result_ransac = global_registration(
             source_down, target_down, source_fpfh, target_fpfh, voxel_size
         )
         rmse.append(result_ransac.inlier_rmse)
-        lst_pcs.append(
-            pcd2np_point_cloud(source.transform(result_ransac.transformation))
-        )
-        # print(result_ransac.inlier_rmse)
-    return lst_pcs, np.array(rmse)
+
+    return np.array(rmse)
 
 
 def compute_surface_area(pcd, voxel_size):
@@ -100,9 +99,12 @@ def compute_weights(reference_pcd, point_clouds, accuracies, voxel_size):
     reference_surface_area = compute_surface_area(reference_pcd_ds, voxel_size * 2)
 
     weights = []
+    point_clouds_pcd = []
 
     for pc_idx in range(len(point_clouds)):
         pcd = np_point_cloud2_pcd(point_clouds[pc_idx])
+        point_clouds_pcd.append(pcd)
+
         pcd_ds = pcd.voxel_down_sample(voxel_size=voxel_size * 2)
 
         pcd_surface_area = compute_surface_area(pcd_ds, voxel_size * 2)
@@ -116,6 +118,6 @@ def compute_weights(reference_pcd, point_clouds, accuracies, voxel_size):
         weights = np.sum(weights) - weights
 
     # weights_ = weights / np.sum(weights)
-    lst_PCs, rmse = compute_rmse(point_clouds, accuracies, voxel_size)
+    rmse = compute_rmse(point_clouds_pcd, accuracies, voxel_size)
     g_weight = weights / (1 - rmse)
     return g_weight
